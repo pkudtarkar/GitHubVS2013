b117b2f8f7b8c858ad49ffe56784c9de19ca5744
kudtarkarp <pradnya.kudtarkar@investors.com>
2016-02-02 11:37:07 -0800

    Testing Project

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ConsoleApplication2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string namestr;
-            namestr = "Pradnya";
-            Console.WriteLine(namestr);
         }
     }
 }
